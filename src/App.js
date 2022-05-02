a38bc8841eec25450eb0bb6d6677fd45f5f36178
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2022-05-02 07:09:26 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 
-import './App.css';
+import { Fragment } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import About from "./pages/Home/About/About";
+import Header from './Pages/Shared/Header/Header';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home/Home";
 
 function App() {
   return (
     <div className="">
-      
+      <Fragment>
+        <Header>
+          <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          </Routes>
+        </Header>
+      </Fragment>
     </div>
   );
 }
